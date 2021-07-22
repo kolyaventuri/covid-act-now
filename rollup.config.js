2752111d283ee26e29b532b6d063e4fbfa132742
Kolya Venturi <me@kolya.co>
2021-07-22 08:32:00 -0700

    Rollup updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
+import {terser} from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
   output: {
     dir: 'lib',
     format: 'umd',
-    name: 'covidActNow'
+    name: 'covidActNow',
+    plugins: [terser()]
   },
   plugins: [typescript()]
 }
