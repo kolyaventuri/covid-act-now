d1bfee9b93137c2dd06c5c39a4d6fd0c2936f475
Kolya Venturi <kolya.venturi@enx3s.com>
2021-07-22 19:16:59 -0700

    Allow scope for urls

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,7 +3,7 @@ import context from '../context';
 import {Scope} from '../types';
 
 export const buildUrl = (name: string, scope: Scope | null = null): string => {
-  console.log(scope);
   const {key} = context.getState() as {key: string};
-  return `${BASE_URL}/${name}.json?apiKey=${key}`;
+  const scopePrefix = scope ? `${scope}/` : '';
+  return `${BASE_URL}${scopePrefix}${name}.json?apiKey=${key}`;
 };
--- test/utils/url.test.ts
+++ test/utils/url.test.ts
@@ -7,5 +7,10 @@ context.set('key', 'apikey');
 
 test('#buildUrl returns the right URL', t => {
   const result = buildUrl('data');
-  t.is(result, `${BASE_URL}/data.json?apiKey=apikey`);
+  t.is(result, `${BASE_URL}data.json?apiKey=apikey`);
+});
+
+test('#buildUrl can return a scoped URL', t => {
+  const result = buildUrl('data', 'cbsa');
+  t.is(result, `${BASE_URL}cbsa/data.json?apiKey=apikey`);
 });
